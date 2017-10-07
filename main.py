4a173b46bedaf5a92e5cc4a6c0183616a4ea2662
Daria <nowickadaria1@gmail.com>
2017-10-07 14:03:16 +0200

    Added one more calling of print function

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ def print_hello(date):
 if __name__ == '__main__':
     print_hello("friday, 06.10.2017")
     print_hello("friday, 06.10.2017 - added .gitignore")
+    print_hello("saturday, 07.10.2017")
